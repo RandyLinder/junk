a37144087ec4c451e66087fcfe60b0656c42cb22
Randy Linder <rlinder@austin.utexas.edu>
2013-10-24 13:20:16 -0500

    first set of programming files

---
+++ Berrio.Chapter3.py
@@ -0,0 +1,292 @@
+# Exercises Chapter 3
+
+# Programming Exercises
+
+# 1
+# Area_Vol.py
+# 	A program to calculate surface Area and Volume of a sphere
+# by: Alejandro Berrio
+
+
+def main():
+	import math
+	print('Hello, this program calculates areas and volumes of spheres.')
+	radius = eval(input('What is the radius of the sphere? '))
+	Vol = 4/3 * math.pi * radius**3
+	Area = 4 * math.pi * radius**2
+	print('The Area is', Area, 'and the Volume is ',Vol)
+
+
+main()
+
+
+#2
+# pizza.py
+# 	A program to calculate the cost per square inch of a circular pizza
+# by: Alejandro Berrio
+
+
+def main():
+	import math
+	print('Hello, this program calculates the cost per square inch of a circular pizza.')
+	dia = eval(input('What is the diameter of the pizza? ')) 
+	price = eval(input('what is the price of the pizza?'))
+	radius = dia / 2
+	Area = math.pi * radius**2
+	cost = price / Area
+	print('The cost of the pizza is', cost, 'dollar per square inch')
+
+main()
+
+# 3
+# molweigth.py
+# 	A program to calculate the molecular weight of a hydrocarbon
+# by: Alejandro Berrio
+
+
+def main():
+	print('Hello, this program calculates the molecular weight of a hydrocarbon.')
+	h,c,o = eval(input('How many H, C and O are in the hydrocarbon? '))
+	molweight = h*1.0079 + c*12.011 + o*15.9994
+	print('The molecular weight of a hydrocarbon is', molweight)
+
+
+main()
+
+
+# 4
+# distance.py
+# 	A program to calculate the distance to a lightning strike
+# by: Alejandro Berrio
+
+
+def main():
+	print('Hello, this program calculates the distance to a lightning strike')
+	time = eval(input('What is the time (in seconds) elapsed between the ﬂash and the sound of thunder? '))
+	dist = time * 1100
+	distance = dist / 5280
+	print('The distance to a lightning strike is', distance, 'miles')
+
+
+main()
+
+
+
+#5
+# coffee.py
+# 	A program to calculate the cost of an order of coffee
+# by: Alejandro Berrio
+
+
+def main():
+	print('Hello, this program calculates the cost of an order of coffee at $10.50 a pound plus shipping for $0.86 per pound + $1.50 ﬁxed cost for overhead')
+	coffee = 10.5
+	shipping = 0.86	
+	overhead = 1.5
+	order = eval(input('How many pounds of coffee do you want to order? '))
+	cost = (order * (coffee + shipping + overhead))
+	print('the cost of your order is : $',cost)
+	
+		
+
+main()
+
+#6
+# slope.py
+# 	A program to calculate the slope of a line 
+# by: Alejandro Berrio
+
+
+def main():
+	print('Hello, this program calculates the slope of a line ')
+	x1,y1 = eval(input('What are the the coordinates of point 1? '))
+	x2,y2 = eval(input('What are the the coordinates of point 2? '))
+	slope = (y2 - y1) / (x2 - x1)
+	print('The slope of the line is', slope)
+
+main()
+
+
+#7
+# distance.py
+# 	A program to calculate the slope of a line 
+# by: Alejandro Berrio
+
+def main():
+	import math
+	print('Hello, this program determines the distance between two points ')
+	x1,y1 = eval(input('What are the the coordinates of point 1? '))
+	x2,y2 = eval(input('What are the the coordinates of point 2? '))
+	distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+	print('The distance between the points is', distance)
+main()
+
+#8
+# epact.py
+# 	A program to calculate the epact
+# by: Alejandro Berrio
+
+def main():
+	print('Hello, this program determines the epact ')
+	year = eval(input('What is the year? '))
+	C = year // 100
+	epact = (8 + (C//4) - C + (((8 * C) + 13)//25) + 11*(year % 19))%30
+	print('The epact of the year', year, 'is', epact)
+main()
+
+
+#9
+# triangle.py
+# 	A program to calculate the area of a triangle
+# by: Alejandro Berrio
+
+def main():
+	import math
+	print('Hello, this program determines the area of a triangle ')
+	a,b,c = eval(input('Which are the leghts of the three sides of the triangle? '))
+	s = ((a + b + c) / 2)
+	area = math.sqrt(s * (s-a) * (s-b) * (s-c))
+	print('The area of the triangle is', area)
+main()
+
+
+#10
+# ladder.py
+# 	A program to calculate the lenght of a ladder
+# by: Alejandro Berrio
+
+def main():
+	import math
+	print('Hello, this program calculates the lenght of a ladder ')
+	height,angle = eval(input('Which are the height and angle (degrees) of the ladder? '))
+	anglerad = math.pi * angle / 180
+	lenght = height / math.sin(anglerad)
+	print('The lenght of the ladder is ', lenght)
+main()
+
+
+
+#11
+# natural.py
+# 	A program to findthe sum of the first n natural numbers
+# by: Alejandro Berrio
+
+def main():
+	import math
+	print('Hello, this program finds the sum of the first n natural numbers ')
+	n = eval(input('Please enter a number? '))
+	c = n+1
+	x=list(range(1,c))
+	sum = math.fsum(x)		
+	print('The sum of the first',n,'numbers is ', sum)
+main()
+
+
+
+#12
+# naturalcubes.py
+# 	A program to find the sum of the first n natural numbers
+# by: Alejandro Berrio
+
+def main():
+	print('Hello, this program finds the sum of the cubes of the first n natural numbers ')
+	n = eval(input('Please enter a number? '))
+	c = n+1
+	x=list(range(1,c))
+	a= 0
+	for i in x:
+		q = i**3
+		sum = a + q
+		a = q
+	print('The sum of the first ',n,'cubes is ', sum)
+main()
+
+#13
+# naturalcubes.py
+# 	A program to find the sum of a series entered by the user
+# by: Alejandro Berrio
+
+def main():
+	import math
+	print('Hello, this program finds the sum of the series you enter ')
+	n = list(eval(input('Please enter a number series? ')))
+	sum = math.fsum(n)
+	print('The sum of the series',n,'is ', sum)
+main()
+
+#14
+# average.py
+# 	A program to find the sum of the first n natural numbers
+# by: Alejandro Berrio
+
+def main():
+	import math
+	print('Hello, this program finds the average of the series you enter ')
+	n = list(eval(input('Please enter a number series? ')))
+	sum = math.fsum(n)
+	add = 0
+	for i in n:
+		add = add + 1
+	average = sum / add
+	print('The average of the series',n,'is ', average)
+main()
+
+
+#15
+# pi.py
+# 	A program to find pi using series
+# by: Alejandro Berrio
+
+def main():
+	import math
+	print('Hello, this program finds pi ')
+	pi= 0	
+	value = 1
+	n = eval(input('Please enter a number n? '))
+	for i in range(1, n, 2):
+		serie = value * (4/i)
+		pi = pi + serie
+		value = -value
+	diff = math.pi - pi
+	print('pi equals', pi,' and the accuracy is:', diff)
+		
+main()
+
+#16
+# fibonacci.py
+# 	A program to find the fibonacci nth number
+# by: Alejandro Berrio
+
+def main():
+	print('Hello, this program finds the nth fibonacci ')
+	f1 = 1	
+	f2 = 1
+	n = eval(input('Please enter a number n? '))
+	for i in range(2, n):
+		f1, f2 = f2, f1 + f2
+		print(f1,f2)
+	print('the nth Fibonacci number is:', f2)
+
+main()
+
+#17
+# sqrt.py
+# 	A program to find the square root of x
+# by: Alejandro Berrio
+
+def main():
+	import math
+	print('Hello, this program finds the square root of x')
+	x = eval(input('Please enter a number x? '))
+	t = eval(input('Please enter a time t to improve the guess? '))
+	guess = (x / 2)
+	for i in range(1,t+1):
+		guess = ((guess + (x / guess)) / 2)
+		print(guess)
+	sqrt = math.sqrt(x)
+	diff = (guess - sqrt)
+	print('the square root of:', x,' is:',guess,' and the accuracy is ',diff)
+
+
+main()
+
---
+++ Dixon.ch3.py
@@ -0,0 +1,295 @@
+##1.
+
+#sphere_area_vol.py
+#script to calculate the area and volume of a sphere
+import math
+def main():
+    print('This script calculates the area and volume of a sphere')
+    radius = eval(input("Enter the radius: "))
+    pi = math.pi
+    volume = 4/3*pi*radius**3
+    area = 4*pi*radius**2
+    print('area: {}'.format(area))
+    print('volume: {}'.format(volume))                                       
+main()
+
+
+##2.
+
+#price_pizza.py
+import math
+def main():
+    print('This script calculates price per square inch of a pizza')
+    radius = eval(input("Enter the diameter: "))/2
+    cost = eval(input("Enter the price: "))
+    pi = math.pi
+    area = pi*radius**2
+    per_square = cost/area
+    print(per_square)
+                                    
+main()
+
+##3.
+
+import math
+def main():
+    print('This calculates the molecular mass of a hydrocarbon')
+    H = eval(input("Enter the number of hydrogen atoms "))*1.0079
+    C = eval(input("Enter the number of carbon atoms "))*12.011
+    O = eval(input("Enter the number of oxygen atoms "))*15.9994
+    mass_list = [H,C,O]
+    total = sum(mass_list)
+    print("The mass of the molecule is {}".format(total))
+                             
+main()
+
+
+##4.
+import math
+def main():
+    print('This calculates the distance of a lighting strike')
+    time = eval(input("Enter the time between flash and report "))
+    distance = 1100*time / 5280
+    print('the lighting struck {} miles away'.format(distance))
+
+main()
+
+##5.
+import math
+def main():
+	print('This calculates the price of a coffe shipment')
+    mass = eval(input("Enter the weight of the order "))
+    cost = 1.5 + 11.36*mass
+    print("The cost is {}".format(cost))
+
+main()
+
+
+##6.
+
+import math
+def main():
+	print('This script returns the slope of the line between two points')
+    x1 = eval(input("Enter an X coordinate "))
+    y1 = eval(input("Enter an Y coordinate "))
+    x2 = eval(input("Enter an X coordinate "))
+    y2 = eval(input("Enter an Y coordinate "))
+    slope = (y2 - y1) / (x2 = x1)
+    print("The slope is {}".format(slope))
+
+main()
+
+
+##7
+
+import math
+def main():
+	print('This script returns the distance of the line between two points')
+    x1 = eval(input("Enter an X coordinate "))
+    y1 = eval(input("Enter an Y coordinate "))
+    x2 = eval(input("Enter an X coordinate "))
+    y2 = eval(input("Enter an Y coordinate "))
+    distance = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    print("The distance is {}".format(slope))
+
+main()
+
+
+##8.
+
+import math
+def main():
+	print('This script returns the Gregorian epact for a year')
+	year = eval(input("Enter an four digit year "))
+	c = year//100
+	epact = (8 + (c//4) - c + ((8*c + 13)//25) + 11*(year%19))%30
+    print("The epact for year {} is {}".format(year, epact))
+
+main()
+
+
+
+##9.
+def main():
+	print('This script returns the area of a triangle')
+	
+	##get the side lengths
+	
+    a = eval(input("Enter length of first side "))
+    b = eval(input("Enter length of second side "))
+    c = eval(input("Enter length of third side "))
+    
+    ##perform area calculation
+    
+    s = (a+b+c)/2
+    A = sqrt(s*(s-a)*(s-b)*(s-c))
+    print('The area is {}'.format(A))
+    
+    
+##10
+def main():
+	print('This script determines the length a ladder needed to reach a point on a wall')
+    height = eval(input("Enter height of point on wall "))
+    angle = eval(input("Enter the angle the ladder leans at "))
+    length = height/(sin(angle))
+    print('Length of ladder needed is {}'.format(length))
+    
+
+##11
+
+import math
+def main():
+    print('This script returns the sum of the first natural numbers up to a given value')
+    n = eval(input("Enter the nth natural number "))
+    
+    ##build a series of the numbers
+    
+    num_list = list(range(1,(n+1)))
+    answer = sum(num_list)
+    print('the sum is {}'.format(answer))
+    
+main()
+
+##12
+
+import math
+def main():
+    print('This script returns the sum of cubes of the first natural numbers up to a given value')
+    n = eval(input("Enter the nth natural number "))
+    
+    ##build a series of the numbers
+    
+    num_list = list(range(1,(n+1)))
+    print(num_list)
+    answer = 0
+    for i in num_list:
+        answer += i**3
+    print('the sum is {}'.format(answer))
+    
+main()
+
+
+##13
+
+import math
+def main():
+    print('This script sums a given set of numbers')
+    n = eval(input("Enter the how many numbers you'll wanna sum "))
+    num_list = []
+    for i in range(n):
+        x = eval(input("Enter a number "))
+        num_list.append(x)
+    print('the sum is {}'.format(sum(num_list)))
+    
+main()
+
+##14
+#average.py
+#This script averages a series of numbers
+import math
+def main():
+	print('This script averages a set of numbers supplied by the user')
+	n = eval(input("Eneter how many numbers you would like to average")
+	numbers = []
+	for i in range(n):
+		x = eval(input("Please enter a number")
+		numbers.append(x)
+	
+main()
+
+##15
+
+##pi_estimator.py
+
+import math
+print("This script estimates pi with user-supplied precision")
+def main():
+	
+	##get user input
+	
+	n = eval(input("Enter the number of iterations you wish to use"))
+	fractions = []
+	
+	##main loop to assemble fractions to sum
+	
+	for i in range(0,n):
+		x = n + 1
+		fractions.append(4/x)
+		
+	##sum the fractions
+		
+	pi_estimate = sum(fractions)
+	
+	##print results
+	
+	print("Estimate of pi = {}".format(pi_estimate))
+	difference = math.pi - pi_estimate
+	print("Difference bewteen math.pi and estimate = {}".format(difference))
+	
+
+##16
+#Fibanacci.py
+#This script computes the nth Fibanacci number
+
+import math
+print("This script estimates pi with user-supplied precision")
+def main():
+	
+	#get user input
+	
+    n = eval(input("Enter the number of iterations you wish to use "))
+    
+    #set up variables to begin iterating
+    
+    f_numbers = [1]
+    p1 = 1
+    p2 = 0
+    
+    #loop for user supplied n
+    
+    for i in range(n-1):
+            x = p1+p2
+            f_numbers.append(x)
+            p2 = p1
+            p1 = x
+    
+    ##print results
+    
+    print("The F number is {}".format(x))
+main()
+
+
+##17
+#square_root.py
+#This script estimates the square root of a given number
+
+import math
+print("This script estimates the square root of a given number")
+def main(start_guess):
+	
+	#get user input
+	
+    x = eval(input("Enter the number you want to estimate square root of "))
+    iterations = eval(input("Enter the number of iterations you wish to use "))
+    
+    #assign first guess based on argument passed to main function
+    
+    guess = (start_guess + (x/start_guess))/2
+    
+    #iterate
+    
+    for i in range(iterations):
+        guess = (guess + (x/guess))/2
+    difference = abs(math.sqrt(x) - guess)
+    
+    #print results
+    
+    print("Estimated square root of {} is {}".format(x, guess))
+    print("Difference from math.sqrt estimate is {}".format(difference))
+
+main(2)
+		
+
+		
+
+
